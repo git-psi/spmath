bde50e98cd9a4438125e30d99d9c4375c727095e
git-psi <100861705+git-psi@users.noreply.github.com>
2024-11-10 11:46:33 +0100

    Add files via upload

---
+++ includes/hdp_menu.php
@@ -0,0 +1,47 @@
+﻿<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+<head>
+<meta charset="utf-8" />
+<title>SPMath</title>
+<meta name="description" content="Cours de maths à Grenoble" />
+<meta name="keywords" content="grenoble, maths, mathématiques, cours, cours particulier, cours collectif, stages, soutien, perfectionnement, lycée, étudiant, Veronique, Perdu" />
+<meta name="identifier-URL" content="http://www.spmath.com/" />
+<meta name="robots" content="all" />
+<meta name="robots" content="index,follow" />
+<meta name="revisit-after" content="5 days" />
+<meta name="expires" content="never" />
+<meta name="resource-type" content="document" />
+<meta name="author" content="Véronique Perdu" />
+<meta name="copyright" content="Véronique Perdu" />
+<meta name="release" content="V3.2.6" />
+<meta name="viewport" 
+	content="
+	width=device-width, 
+	initial-scale=1,
+	maximum-scale=1,
+	target-densitydpi=device-dpi
+	" />			
+<link rel="stylesheet" type="text/css" href="css/spmath.css" />
+<script type="text/javascript" src="scripts/add_mail.js"></script>
+<noscript>Merci de mettre à jour votre navigateur afin de gérer les scripts.</noscript>
+</head>	
+<body>
+	<div class="site-container">
+			<header class="header">
+				<a href="#" class="header__icon" id="header__icon"><img src="images/hamburger_w.png" width="40" height="40" alt="menu"></img></a>
+				<a href="index.php">SPMath</a>
+			</header>
+		<table class="site-content">
+			<tr class="content">
+				<td>
+					<nav class = "menu">
+						<ul>
+						<li><a href="index.php">ACCUEIL</a></li>
+						<li><a href="index-2.php">QUI ENSEIGNE ?</a></li>
+						<li><a href="index-3.php">COURS PARTICULIERS</a></li>
+						<li><a href="index-4.php">PUBLICATIONS</a></li>
+						<li><a href="index-5.php">LE LIEU ?</a></li>
+						<li><a href="index-6.php">LIENS</a></li>
+						<li><a href="mailto:veronique.perdu@polytechnique.org"><img src="images/courrier.jpeg" width="60" height="70" alt="email"></a></li>
+						</ul>
+					</nav>
+				</td>
