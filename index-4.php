1cdc2532c37b8af15f2c821052688981881a213d
git-psi <100861705+git-psi@users.noreply.github.com>
2024-11-10 11:39:44 +0100

    Add files via upload

---
+++ index-4.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+	<?php include 'includes/hdp_menu.php' ?>
+			<td>
+				<article class="container">
+				
+				Septembre 2024, éditions <a href="https://www.editions-ellipses.fr/accueil/15489-29155-exercices-de-perfectionnement-en-calcul-classes-prepas-9782340094758.html#/1-format_disponible-broche" target="_blank">Ellipses</a><br>
+				<strong>Auteur : </strong>Véronique Perdu
+				<br />
+				Exercices de perfectionnement en calcul – Classes prépas
+<center>
+				<a href="https://www.editions-ellipses.fr/accueil/15489-29155-exercices-de-perfectionnement-en-calcul-classes-prepas-9782340094758.html#/1-format_disponible-broche" target="_blank"><img src="images/livre3.jpg" width="250" height="370"></img></a>
+				</center>
+				<br />
+				<br /><br />
+<br /><br />
+
+
+
+				
+				<br />
+				
+
+				Juillet 2019, éditions <a href="https://www.editions-ellipses.fr/accueil/15051-27943-exercices-de-perfectionnement-en-calcul-niveau-lycee-2e-edition-9782340083424.html#/1-format_disponible-broche" target="_blank">Ellipses</a><br>
+				<strong>Auteur : </strong>Véronique Perdu
+				<br />
+				 Exercices de perfectionnement en calcul - Niveau lycée, prépa
+<center>
+				<a href="https://www.editions-ellipses.fr/accueil/15051-27943-exercices-de-perfectionnement-en-calcul-niveau-lycee-2e-edition-9782340083424.html#/1-format_disponible-broche" target="_blank"><img src="images/livre2.jpg" width="250" height="370"></img></a>
+				</center>
+				<br />
+				<br /><br />
+<br /><br />
+
+
+
+				
+				<br />
+				Octobre 2017, éditions <a href="http://www.generation5.fr/produits/Maths-Lycee-Seconde--Reviser-les-fondamentaux--1611--24305--ens.php" target="_blank">Génération 5</a><br>
+				<strong>Auteurs : </strong>Véronique Perdu, Jean-Michel Béchu
+				<br />
+				 Ouvrage qui vise à faire acquérir des automatismes en calcul en partant des fondamentaux.
+<center>
+				<a href="http://www.generation5.fr/produits/Maths-Lycee-Seconde--Reviser-les-fondamentaux--1611--24305--ens.php" target="_blank"><img src="images/maths_lycee.jpg" width="250" height="370" alt="maths_2nde"></img></a>
+				</center>
+				<br /><br />
+				</article>		<!-- container -->
+			</td>
+	<?php include 'includes/bdp.php' ?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+	<?php include 'includes/hdp_menu.php' ?>
+			<td>
+				<article class="container">
+					SPMath, c'est une <a href="index-2">enseignante</a>, Véronique Perdu, polytechnicienne et agrégée de mathématiques, spécialisée dans le soutien et le perfectionnement en mathématiques de haut niveau pour des élèves de lycée, prépas, universités, étudiants en écoles d'ingénieurs, de commerce...<br /><br />
+					Vous pourrez choisir selon vos besoins des <a href="index-3.php">cours particuliers</a> pour un suivi régulier ou quelques séances ponctuelles avant devoirs, concours, examens...
+					<br /><br />
+					SPMath est <a href="index-5.php">implanté</a> à Saint Martin le Vinoux, à 5 minutes de la porte de France (Esplanade), à 10 minutes du centre de Grenoble.
+					<br /><br />  
+					<p class="contact">contact : <script language="javascript" type="">mail_to('veronique.perdu', 'polytechnique.org');</script> ou  06 63 16 03 76</p>
+					<br />
+					<aside class = "sip">
+						<a href="https://www.impots.gouv.fr/portail/particulier/emploi-domicile" target="_blank"><img src="images/imageSIP.jpg" width="130" height="100" alt="SIP"></img></a>
+						<br />
+						SAP n°484086673
+					</aside>
+				</article>		<!-- container -->
+			</td>
+	<?php include 'includes/bdp.php' ?>
